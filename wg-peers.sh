77844be0c20c2a6467c1f1698f333f11be1f3190
Vista-21 <140960274+Vista-21@users.noreply.github.com>
2026-04-03 01:24:10 +0300

    Update wg-peers.sh

--- wg-peers.sh
+++ wg-peers.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-wg show wg0
+set -e
+
+WG_CONF="/etc/wireguard/wg0.conf"
+
+echo "Peers from $WG_CONF"
+echo
+
+awk '
+  /^
+
+\[Peer\]
+
+/ {peer=1; name=""; pub=""; ip=""}
+  /^# / && peer {name=substr($0,3)}
+  /PublicKey =/ && peer {pub=$3}
+  /AllowedIPs =/ && peer {ip=$3}
+  NF==0 && peer {
+    printf "Name: %-20s  IP: %-18s  PubKey: %s\n", name, ip, pub
+    peer=0
+  }
+' "$WG_CONF"
